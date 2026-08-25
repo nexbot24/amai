39d3629029926fbfbb1a9584374c71b498932f82
JD <jd@JDs-MacBook-Pro.local>
2026-08-25 18:43:12 +0100

    Phase 3+4+5: Mobile template connected to Supabase - services from DB, contact form to requests, rebundled

---
+++ rebundle.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+// Rebundle mobile.html from unpacked_template.html
+// Usage: node rebundle.js
+
+const fs = require('fs');
+const path = require('path');
+
+const dir = __dirname;
+const templatePath = path.join(dir, 'unpacked_template.html');
+const mobilePath = path.join(dir, 'mobile.html');
+
+const template = fs.readFileSync(templatePath, 'utf8');
+const mobile = fs.readFileSync(mobilePath, 'utf8');
+
+// The template is stored as a JSON string between:
+//   <script type="__bundler/template">
+//   </script>
+const startTag = '<script type="__bundler/template">';
+const endTag = '  </script>';
+
+const startIdx = mobile.indexOf(startTag);
+if (startIdx === -1) {
+  console.error('Could not find __bundler/template tag in mobile.html');
+  process.exit(1);
+}
+
+// Find the closing </script> after the template start
+const searchFrom = startIdx + startTag.length;
+const endIdx = mobile.indexOf(endTag, searchFrom);
+if (endIdx === -1) {
+  console.error('Could not find closing script tag after template');
+  process.exit(1);
+}
+
+// Build new mobile.html
+const before = mobile.substring(0, startIdx + startTag.length);
+const after = mobile.substring(endIdx);
+
+const newMobile = before + '\n' + JSON.stringify(template) + '\n  ' + after;
+
+fs.writeFileSync(mobilePath, newMobile, 'utf8');
+console.log('Rebundled mobile.html (' + Math.round(newMobile.length / 1024) + 'KB)');
